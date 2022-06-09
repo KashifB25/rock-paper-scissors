0994942f4530d66f210f694ef7bad880501f5831
KashifB25 <kashifbarbhuiya@gmail.com>
2022-06-09 18:12:56 +0530

    Add function playRound to simulate a round between player and computer

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerChoice = playerSelection.toLowerCase();
+    let winConditon = () => (playerChoice == "rock" && computerSelection == "Scissors") || (playerChoice == "paper" && computerSelection == "Rock") || (playerChoice == "scissors" && computerSelection == "Paper")
+    if (playerChoice === computerSelection.toLowerCase()) {
+        return "It's a tie! Both chose " + playerSelection;
+    }
+    else if (winConditon()) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
