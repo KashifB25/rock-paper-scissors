ddab7d6c7359319655724184183f11dfa65d5ead
KashifB25 <kashifbarbhuiya@gmail.com>
2023-07-29 21:12:02 +0530

    Announce a winner once one player reaches 5 points

--- script.js
+++ script.js
@@ -33,22 +33,66 @@ function game(playerChoice) {
         let res = playRound(playerChoice, getComputerChoice());
         if (res == 1) {
             playerScore++;
+            showRoundResult("You Win!");
             //result.innerHTML += `<br>Round ${round + 1}: You Win!\n`;
         } else if (res == -1) {
             computerScore++;
             //result.innerHTML += `<br>Round ${round + 1}: You Lose!\n`;
+        } 
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+
+        if (playerScore === 5 || computerScore === 5) {
+            // Show the final screen with the winner's result
+            showFinalScreen(playerScore, computerScore);
         } else {
-            //result.innerHTML += `<br>Round ${round + 1}: ` + 'Tie!\n';
+            // Update the round number
+            document.querySelector('.play-page-round').textContent = `Round ${round + 1}`;
+            round++;
         }
-        round++;
 }
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let playerChoice = button.textContent;
         game(playerChoice);
-        playerScoreDisplay.textContent = playerScore;
-        computerScoreDisplay.textContent = computerScore;
-        document.querySelector('.play-page-round').textContent = `Round ${round}`;
     });
 });
+
+function showRoundResult(result) {
+    const roundResultSection = document.querySelector('.play-page-round-result');
+    const roundResultSpan = roundResultSection.querySelector('span');
+    roundResultSpan.textContent = result;
+}
+
+function showFinalScreen(playerScore, computerScore) {
+    const finalScreen = document.querySelector('.play-page-final-screen');
+    const finalScreenText = document.querySelector('.play-page-final-screen__text');
+    const finalScreenScore = document.querySelector('.play-page-final-screen__score');
+
+    // Determine the winner based on the scores
+    let winner = playerScore > computerScore ? 'You Win!' : 'Computer Wins!';
+
+    // Update the final screen text and score display
+    finalScreenText.textContent = winner;
+    finalScreenScore.textContent = `${playerScore} : ${computerScore}`;
+
+    // Show the final screen
+    finalScreen.style.display = 'block';
+}
+
+// Add event listener to reset the game when the final screen is clicked (optional)
+document.querySelector('.play-page-final-screen').addEventListener('click', () => {
+    resetGame();
+});
+
+// Function to reset the game
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 1;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    document.querySelector('.play-page-round').textContent = `Round ${round}`;
+    document.querySelector('.play-page-final-screen').style.display = 'none';
+}
